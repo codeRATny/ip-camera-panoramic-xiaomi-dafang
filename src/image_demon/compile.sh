819fdc8e02e8cfc1dcef2f6f9b54f31bc79d2090
Pavel-Kr <pkrivosheev@bk.ru>
2022-11-29 22:17:40 +0700

    Create project structure

--- src/image_demon/compile.sh
+++ src/image_demon/compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-TOOLCHAIN=$(pwd)/../toolchain/bin
+TOOLCHAIN=$(pwd)/../../toolchain/bin
 CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
 export CROSS_COMPILE=${CROSS_COMPILE}
 export CC=${CROSS_COMPILE}gcc
@@ -7,5 +7,4 @@ export LD=${CROSS_COMPILE}ld
 export CFLAGS="-muclibc -O2 -DDEBUG_TRACE -DFAKE_ROOT "
 export CPPFLAGS="-muclibc -O2"
 export LDFLAGS="-muclibc -O2"
-make clean
 make
